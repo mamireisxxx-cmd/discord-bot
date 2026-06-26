290a663db01afe1fd33559f3ab3afd04dcd7c52e
mamireisxxx-cmd <mamireisxxx@gmail.com>
2026-06-26 16:48:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-main.py
+import discord
+from discord.ext import commands
+import os
 
-print("VOID SYSTEM")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} aktif!")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong!")
+
+TOKEN = os.getenv("TOKEN")
+bot.run(TOKEN)
